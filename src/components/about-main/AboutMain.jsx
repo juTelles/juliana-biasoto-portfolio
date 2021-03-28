93f39228b3e83290ff81198c8280660bd10e353c
juzinha <julianatelles.contato@gmail.com>
2021-03-28 19:48:00 -0300

    about page

--- src/components/about-main/AboutMain.jsx
+++ src/components/about-main/AboutMain.jsx
@@ -1,14 +1,31 @@
 // eslint-disable-next-line no-unused-vars
 import react, { useEffect, useState } from 'react';
-import MyPhoto from '../myphoto/MyPhoto';
+import Paper from '../paper/Paper';
 import styles from './styles.module.css';
+import { aboutText } from './aboutText';
+import Frame from '../frame/Frame';
 
 function AboutMain() {
+  const text = aboutText.pt;
 
   return (
-    <section className="elements-sections main-section">
-      <div className="main-section-div">
-        <MyPhoto />
+    <section>
+      <div className={styles.mainAboutDiv}>
+        <div className={styles.aboutPaperDiv}>
+          <Paper color="#dbc3f3" pinColor="purple" className={styles.paperContentDiv}>
+            <div className={styles.paperHeader}>
+            <h2 className={styles.aboutPaperTitle}>Juliana Biasoto</h2>
+            <Frame/>
+            </div>
+            <div className={styles.paperMainContent}>
+            <p className={styles.aboutPaperText}>{text.text}</p>
+            <div className={styles.bottomFrameAndDate}>
+            <Frame rotate="180deg"/>
+            <p>{text.date}</p>
+            </div>
+            </div>
+          </Paper>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/about-main/aboutText.js
@@ -0,0 +1,29 @@
+export const aboutText = {
+  pt: {
+      title: 'Sobre mim',
+      type: 'oneText',
+      text: `Eu amo animais e seres humanos, já li tantos livros que confundo e
+         misturo seus personagens, gosto de humor inteligente e jogos que
+         desafiam o cérebro. Acho que a simpatia e civilidade são essenciais em
+         qualquer ambiente, contanto que a verdade e o pensamento crítico
+         também sejam uma presença constante. Acho importante estar em
+         constante desenvolvimento, nesse mundo tão impressionante e complexo
+         em que vivemos sempre a algo novo a aprender e conhecimentos antigos a
+         relembrar e melhorar.`,
+      date: '15 de Fevereiro de 2020'
+  },
+  en: {
+      title: 'sobre mim',
+      type: 'oneText',
+      text: `I love animals and human beings,
+         I have read so many books that I mix up their characters,
+         I like intelligent humor and games that challenge the brain.
+         I think sympathy and civility are essential to any environment
+         as long as truth and critical thinking remain always present as well.
+         I think it is important to be in constant development,
+         in this impressive and complex world we live in,
+         there is always something new to learn and old knowledge
+         to be remembered and improved.`,
+      date: '02/15/2020',
+  },
+};
