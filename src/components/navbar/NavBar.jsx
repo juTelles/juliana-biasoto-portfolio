65018e17db0c52f1a2af480ee2abde8712f8e4bd
juzinha <julianatelles.contato@gmail.com>
2021-03-28 19:50:12 -0300

    navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,18 +1,48 @@
 // eslint-disable-next-line no-unused-vars
 import react, { useState } from 'react';
+import MenuNotesBackground from '../menu-back-notes/MenuBackNotes';
 import NavNotes from '../navnotes/NavNotes';
-import styles from './styles.module.css'
+import PinnedNote from '../pinned-note/PinnedNote';
+import styles from './styles.module.css';
 
-function NavBar({ resume, about, portfolio, home, isHome }) {
+function NavBar({ resume, about, portfolio, home, contact }) {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleClickMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
-    <nav className={isHome? styles.navBarHome : styles.navBar}>
-      <NavNotes
-        resume={resume}
-        about={about}
-        portfolio={portfolio}
-        home={home}
-        isHome={isHome}
-      />
+    <nav className={styles.navBar}>
+      <div className={styles.menuDiv}>
+          <PinnedNote
+            text="Menu"
+            noteName="menu"
+            onClick={handleClickMenu}
+            rotate="1deg"
+            margin="-7px auto"
+          />
+        {!menuIsOpen && (
+            <MenuNotesBackground
+              home={home}
+              about={about}
+              resume={resume}
+              portfolio={portfolio}
+              contact={contact}
+            />
+        )}
+      </div>
+      {menuIsOpen && (
+        <div className={styles.navNotesDiv}>
+          <NavNotes
+            resume={resume}
+            about={about}
+            portfolio={portfolio}
+            home={home}
+            contact={contact}
+          />
+        </div>
+      )}
     </nav>
   );
 }
