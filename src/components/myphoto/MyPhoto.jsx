5c87a7944de2cbd91f9667785ee89b15f24f945f
juzinha <julianatelles.contato@gmail.com>
2021-03-28 19:51:01 -0300

    my photo component

--- src/components/myphoto/MyPhoto.jsx
+++ src/components/myphoto/MyPhoto.jsx
@@ -1,17 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import react from 'react';
-import myPhoto from '../../img/pollaroid-ju.png';
+import myPhoto from '../../img/polaroid-ju.png';
 import styles from './styles.module.css';
 
-function MyPhoto() {
+function MyPhoto({ margin, rotate, width }) {
   return (
-    <div className={styles.photoDiv}>
+    <div
+      className={styles.photoDiv}
+      style={{ margin: margin, transform: `rotate(${rotate})`, width: width }}
+    >
       <figure className={styles.photoFig}>
-        <img
-          className={styles.photo}
-          src={myPhoto}
-          alt="Juliana Biasoto"
-        />
+        <img className={styles.photo} src={myPhoto} alt="Juliana Biasoto" />
+        <div className={styles.photoShadow}></div>
       </figure>
     </div>
   );
