48fad0d1e31fcd33088646dbe4c37356f35ff262
juzinha <julianatelles.contato@gmail.com>
2021-03-28 19:49:07 -0300

    home page

--- src/components/home-components/home-main/HomeMain.jsx
+++ src/components/home-components/home-main/HomeMain.jsx
@@ -1,17 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import react, { useEffect, useState } from 'react';
-import NavBar from '../navbar/NavBar';
-import MyPhoto from '../myphoto/MyPhoto';
+import MyPhoto from '../../myphoto/MyPhoto';
 import styles from './styles.module.css';
-
+import HomeNavBar from '../home-navbar/HomeNavBar';
 
 function HomeMain() {
 
   return (
-    <section className="elements-sections main-section">
-      <div className="main-section-div">
+    <section>
+      <div className={styles.homeMainDiv}>
         <MyPhoto />
-        <NavBar resume about portfolio isHome/>
+        <HomeNavBar isHome />
       </div>
     </section>
   );
---
+++ src/components/home-components/home-navbar/HomeNavBar.jsx
@@ -0,0 +1,53 @@
+// eslint-disable-next-line no-unused-vars
+import react from 'react';
+import StickNote from './../../sticknote/StickNote';
+import styles from './styles.module.css';
+import { Link } from 'react-router-dom';
+
+function HomeNavBar({ isHome }) {
+
+  return (
+     <div className={styles.homeNavNotesDiv}
+  >
+      <Link to="/about" className={styles.noteLink}>
+        <StickNote
+          isHome={isHome}
+          text="Sobre Mim"
+          noteName="about"
+          color="#fffa92"
+          rotate="2deg"
+        />
+      </Link>
+      <Link to="/resume" className={styles.noteLink}>
+        <StickNote
+          isHome={isHome}
+          text="Currículo"
+          noteName="resume"
+          color="#f8bed8"
+          rotate="-2deg"
+        />
+      </Link>
+      <Link to="/portfolio" className={styles.noteLink}>
+        <StickNote
+          isHome={isHome}
+          text="Portifólio"
+          noteName="portfolio"
+          color="#d5f8a5"
+          rotate="2deg"
+        />
+      </Link>
+      <Link to="/contact" className={styles.noteLink}>
+        <StickNote
+          isHome={isHome}
+          text="Contato"
+          noteName="contact"
+          color="#c8fbfc"
+          rotate="-2deg"
+
+        />
+      </Link>
+
+  </div>
+  )}
+
+export default HomeNavBar;
