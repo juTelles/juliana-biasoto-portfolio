42fed62ca06f74faa7cb5d3cdba7b3cb239a5791
juzinha <julianatelles.contato@gmail.com>
2021-04-25 17:13:15 -0300

    some fixes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,7 +5,7 @@ import PortfolioMain from '../components/portfolio-main/PortfolioMain';
 function portfolio() {
   return (
     <main>
-      <Header title="Portfolio" className="portfolio-header" home about resume />
+      <Header title="Portfolio" className="portfolio-header" home about resume contact/>
       <PortfolioMain/>
       <Footer />
     </main>
