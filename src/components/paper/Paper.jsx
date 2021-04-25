293b44ade17dfd4b6d17b71894ac3e615825a0f3
juzinha <julianatelles.contato@gmail.com>
2021-04-25 16:45:45 -0300

    paper component fixes

--- src/components/paper/Paper.jsx
+++ src/components/paper/Paper.jsx
@@ -3,20 +3,33 @@ import react, { Children, useEffect, useState } from 'react';
 import Pin from '../pin/Pin';
 import styles from './styles.module.css';
 
-function Paper({ children, title, color, pinColor, className }) {
+function Paper({
+  children,
+  title,
+  color,
+  pinColor,
+  sectionClassName,
+  textClassName,
+  paperSize,
+}) {
   // eslint-disable-next-line no-unused-vars
 
   return (
-    <div className={styles.paperDiv}>
-      <div className={styles.paper} style={{backgroundColor: color}}>
+    <div
+      style={{ width: paperSize }}
+      className={`${styles.paperDiv} ${styles[sectionClassName]}`}
+    >
+      <div className={styles.paper} style={{ backgroundColor: color }}>
         <div className={styles.pinsDiv}>
-          <Pin className={styles.pin} color={pinColor}/>
-          <Pin className={styles.pin} color={pinColor}/>
+          <Pin className={styles.pin} color={pinColor} />
+          <Pin className={styles.pin} color={pinColor} />
         </div>
         <div className={styles.paperHeaderDiv}>
           <h2 className={styles.paperTitle}>{title}</h2>
         </div>
-        <div className={`${styles.paperContentDiv} ${className}`}>
+        <div
+          className={`${styles.paperContentDiv} ${styles[sectionClassName]}`}
+        >
           {children}
         </div>
       </div>
